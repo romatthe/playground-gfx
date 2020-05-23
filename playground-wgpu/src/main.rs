0f3978a267ecfed9fde0248b9637cc37c8400363
Robin Mattheussen <robin.mattheussen@gmail.com>
2020-05-23 17:42:32 +0200

    Created a winit window!

--- playground-wgpu/src/main.rs
+++ playground-wgpu/src/main.rs
@@ -1,3 +1,32 @@
+use winit::event_loop::{EventLoop, ControlFlow};
+use winit::window::WindowBuilder;
+use winit::event::{Event, WindowEvent, ElementState, KeyboardInput, VirtualKeyCode};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { ref event, window_id} if window_id == window.id() => {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        match input {
+                            KeyboardInput {
+                               state: ElementState::Pressed,
+                               virtual_keycode: Some(VirtualKeyCode::Escape),
+                               ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => ()
+                        }
+                    }
+                    _ => ()
+                }
+            },
+            _ => ()
+        }
+    });
 }
